737ba7d2b54f9ed56b2388c439cff5636b206069
Venic <sashabratchikov72@gmail.com>
2025-02-19 00:01:43 +0300

    domain

---
+++ Magazine.Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Magazine.Domain
+{
+    public class Product
+    {
+        Guid id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string ImageSrc { get; set;}
+    }
+}
---
+++ Magazine.Domain/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine.Domain.Services
+{
+    internal interface IProductService
+    {
+        Product Add();
+        Product Remove(Product product);
+        Product Edit(Product product);
+        Product Search();
+    }
+}
