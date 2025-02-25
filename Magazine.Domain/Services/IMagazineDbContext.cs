2c9ac15a4e98e52dcf6193b515759f7d3014d935
Venic <sashabratchikov72@gmail.com>
2025-02-25 23:58:32 +0300

    db connect

---
+++ Magazine.Domain/Services/IMagazineDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine.Domain.Services
+{
+    public interface IMagazineDbContext
+    {
+        DbSet<Product> Product_ { get; set; }
+    }
+}
---
+++ Magazine.Persistance/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Magazine.Domain;
+using Magazine.Domain.Services;
+using Magazine.Domain.Commands;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace Magazine.Persistance
+{
+    
+    public class ProductService:IProductService
+    {
+        private readonly IMagazineDbContext _MagazineDbContext;
+       public Product Add(AddCommand addCommand)
+        {
+            var prod = new Product
+            {
+                id = Guid.NewGuid(),
+                Name = addCommand.Name,
+                Description = addCommand.Description,
+                Price = addCommand.Price,
+                ImageSrc = addCommand.ImageSrc
+            };
+           _MagazineDbContext.Product_.Add(prod);
+            return prod;
+        }
+       public Product Remove(Product remuvble)
+        {
+            return prod;
+        }
+       public Product Edit(Product editble)
+        {
+            return prod;
+        }
+       public Product Search(Product searchble)
+        {
+            return prod;
+        }
+    }
+}
